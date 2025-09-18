94650ea4ba8a49417ae4479c4bf1325884cc935f
Miguel <rojassucarinomiguelantonio@gmail.com>
2025-09-18 12:42:23 -0400

    Making a method secure

--- src/main/java/com/security/test/controller/userController.java
+++ src/main/java/com/security/test/controller/userController.java
@@ -1,9 +1,15 @@
 package com.security.test.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("s1")
 public class userController {
+
+    @GetMapping("/index")
+    public String index() {
+        return "endPoint secured";
+    }
 }
