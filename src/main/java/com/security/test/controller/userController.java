e1b4460f8f73fa3ce374bfd202a0b0c2c1ab2cad
Miguel <rojassucarinomiguelantonio@gmail.com>
2025-09-18 12:39:02 -0400

    Making the annotations

--- src/main/java/com/security/test/controller/userController.java
+++ src/main/java/com/security/test/controller/userController.java
@@ -1,4 +1,9 @@
 package com.security.test.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("s1")
 public class userController {
 }
