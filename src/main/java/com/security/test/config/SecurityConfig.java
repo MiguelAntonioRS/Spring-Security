b4a9d2138d59a78ccbb2cfa20dc43a4d4217e03c
Miguel <rojassucarinomiguelantonio@gmail.com>
2025-09-20 11:12:22 -0400

    Creating the method to config for security

--- src/main/java/com/security/test/config/SecurityConfig.java
+++ src/main/java/com/security/test/config/SecurityConfig.java
@@ -1,9 +1,16 @@
 package com.security.test.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
+
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .build();
+    }
 }
