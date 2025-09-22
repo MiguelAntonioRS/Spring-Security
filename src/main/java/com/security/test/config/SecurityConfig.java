235f2b2f37ee5a29d2125643a5d9ccce8dc0a203
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2025-09-22 11:25:58 -0400

    Changing name of variable

--- src/main/java/com/security/test/config/SecurityConfig.java
+++ src/main/java/com/security/test/config/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig {
                 .build();*/
 
         return httpSecurity
-                .authorizeHttpRequests(authz -> authz
+                .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/s1/notSecured").permitAll()
                         .anyRequest().authenticated()
                 )
