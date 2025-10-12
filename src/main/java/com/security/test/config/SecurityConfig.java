c58fb137506e49dab84a1e6e7c16cd01b2ed6017
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2025-10-12 10:41:18 -0400

    Written a @Bean in the method AuthenticationProvider

--- src/main/java/com/security/test/config/SecurityConfig.java
+++ src/main/java/com/security/test/config/SecurityConfig.java
@@ -51,6 +51,7 @@ public class SecurityConfig {
         return authenticationConfiguration.getAuthenticationManager();
     }
 
+    @Bean
     public AuthenticationProvider authenticationProvider() {
 
     }
