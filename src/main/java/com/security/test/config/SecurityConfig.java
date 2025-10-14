1fb7839a6b69354c253067a45e8b702814652844
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2025-10-14 10:37:32 -0400

    Finishing the UserDetailsService method

--- src/main/java/com/security/test/config/SecurityConfig.java
+++ src/main/java/com/security/test/config/SecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -73,6 +74,8 @@ public class SecurityConfig {
                 .roles("ADMIN")
                 .authorities("READ", "CREATE")
                 .build();
+
+        return new InMemoryUserDetailsManager();
     }
 
     @Bean
