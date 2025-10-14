206cd92c43781b32afe14003e2e0b4c850e5bb1b
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2025-10-14 10:23:26 -0400

    Returning NoOpPasswordEncoder

--- src/main/java/com/security/test/config/SecurityConfig.java
+++ src/main/java/com/security/test/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -64,6 +65,6 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-
+        return NoOpPasswordEncoder.getInstance();
     }
 }
