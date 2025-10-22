73ca4cae9de8034c7bce902482d06eadf9246949
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2025-10-22 11:32:26 -0400

    Configuring the security in the controller

--- src/main/java/com/security/test/controller/UserController.java
+++ src/main/java/com/security/test/controller/UserController.java
@@ -1,11 +1,13 @@
 package com.security.test.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
+@PreAuthorize("denyAll()")
 public class UserController {
 
 
