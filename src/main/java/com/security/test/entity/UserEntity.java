d4f863d0bb151c597a137be941ebe0fce2d080f3
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2025-10-23 13:07:25 -0400

    Putting Annotations in the entity class

--- src/main/java/com/security/test/entity/UserEntity.java
+++ src/main/java/com/security/test/entity/UserEntity.java
@@ -1,4 +1,15 @@
 package com.security.test.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
 public class UserEntity {
 }
