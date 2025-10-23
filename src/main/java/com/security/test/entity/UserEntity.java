006ffb4633f13e369bed2694dae7a48b486bdf39
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2025-10-23 13:09:04 -0400

    Creating the id attribute in the entity class

--- src/main/java/com/security/test/entity/UserEntity.java
+++ src/main/java/com/security/test/entity/UserEntity.java
@@ -1,7 +1,6 @@
 package com.security.test.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 
 @AllArgsConstructor
@@ -12,4 +11,8 @@ import lombok.*;
 @Entity
 @Table(name = "users")
 public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
