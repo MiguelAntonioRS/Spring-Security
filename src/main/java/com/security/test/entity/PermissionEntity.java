97d063191f6390f28f277762830db08331d207b6
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2025-10-26 11:08:01 -0400

    Creating conditions for the attributes

--- src/main/java/com/security/test/entity/PermissionEntity.java
+++ src/main/java/com/security/test/entity/PermissionEntity.java
@@ -16,6 +16,6 @@ public class PermissionEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true, nullable = false)
+    @Column(unique = true, nullable = false, updatable = false)
     private String name;
 }
