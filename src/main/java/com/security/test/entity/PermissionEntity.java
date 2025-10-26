7bcbe40a682cff9ac60d965b1494beb89e462d07
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2025-10-26 11:05:58 -0400

    Adding annotations

--- src/main/java/com/security/test/entity/PermissionEntity.java
+++ src/main/java/com/security/test/entity/PermissionEntity.java
@@ -1,5 +1,16 @@
 package com.security.test.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+@Table(name = "permissions")
 public class PermissionEntity {
 
     private Long id;
